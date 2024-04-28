7ce29cf4d649ec8e04281c8ccb411b63138f23f6
Proper <74909931+Properprogress1@users.noreply.github.com>
2024-04-28 02:25:53 +0100

    CI/CD integration

---
+++ src/main/java/com/example/rupstore/RupStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.rupstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RupStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RupStoreApplication.class, args);
+    }
+
+}
